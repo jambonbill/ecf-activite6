871555958138a71196d2b26902ab60f098f1a60f
orange <jambonbill@gmail.com>
2023-06-08 16:42:37 +0200

    src

---
+++ src/App/Controllers/InstrumentController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controllers;
+
+use Exceptions;
+
+use Twig;
+
+use App\Models\Instrument;
+use App\Models\InstrumentManager;
+
+class InstrumentController
+{
+    
+    private $templateFolder=__DIR__."/../../../public/templates";
+    private $twig = null;
+    
+    public function __construct()
+    {
+        $twigloader=new Twig\Loader\FilesystemLoader($this->templateFolder);
+        $this->twig=new Twig\Environment($twigloader);
+    }
+
+
+    public function index()
+    {
+        $manager=new InstrumentManager();
+        $entities=$manager->findAll();
+        //var_dump($entities);
+        $template=$this->twig->load('instruments.twig');        
+        
+        echo $template->render([
+            "title"=>"Instruments",
+            "entities"=>$entities
+        ]);
+
+    }
+	
+	
+}
